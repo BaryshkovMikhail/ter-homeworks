8d129a6e9c1acd0b3be4b61b0b53f213d77f5508
Baryshkov Mikhail <m.baryshkov@mail.ru>
2025-07-21 16:41:39 +0300

    второе и третье готово

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,42 +1,91 @@
+# VPC Network
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
-resource "yandex_vpc_subnet" "develop" {
-  name           = "${var.vpc_name}-subnet"
-  zone           = var.default_zone
+
+# Подсеть в зоне ru-central1-a
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "${var.vpc_name}-subnet-a"
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
 
-
-data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+# Подсеть в зоне ru-central1-b
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "${var.vpc_name}-subnet-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.2.0/24"] # Второй диапазон IP
 }
-resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v1"
+
+# Источник образа Ubuntu
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_image_family
+}
+
+# Первая ВМ: Web
+resource "yandex_compute_instance" "web" {
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
+
   resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
+
   network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
+    subnet_id = yandex_vpc_subnet.develop_a.id
     nat       = true
   }
 
   metadata = {
     serial-port-enable = 1
-    ##ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-    ssh-keys = "ubuntu:${file("~/.ssh/woland2.pub")}"
+    ssh-keys           = "ubuntu:${file("~/.ssh/woland2.pub")}"
+  }
+}
+
+# Вторая ВМ: DB
+resource "yandex_compute_instance" "db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
   }
 
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_b.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${file("~/.ssh/woland2.pub")}"
+  }
+
+  zone = var.vm_db_zone
 }
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,38 +1,34 @@
-###cloud vars
-
+### Cloud vars
 
 variable "cloud_id" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  description = "Cloud ID in Yandex Cloud"
 }
 
 variable "folder_id" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  description = "Folder ID in Yandex Cloud"
 }
 
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
 variable "vpc_name" {
   type        = string
   default     = "develop"
-  description = "VPC network & subnet name"
 }
 
-
-###ssh vars
+### SSH vars
 
 variable "vms_ssh_root_key" {
   type        = string
   default     = "~/.ssh/woland2.pub"
-  description = "ssh-keygen -t ed25519"
+  description = "SSH public key for root access"
 }
